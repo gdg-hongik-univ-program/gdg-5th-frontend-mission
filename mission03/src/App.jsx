ce4ce19121272ca59307b3b5631cd68ccedc6c49
seokyun9 <westse05@gmail.com>
2025-11-09 19:12:31 +0900

    submit : week1

---
+++ mission03/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Content from './components/Content'
+
+
+function App() {
+
+  return (
+    <><div>
+    <Navbar />
+    <Content />
+    <Footer />
+    </div>
+    </>
+  )
+}
+
+export default App
